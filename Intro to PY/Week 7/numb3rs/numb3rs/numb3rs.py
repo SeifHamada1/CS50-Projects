e85d3dfd6d3c0538b3fb92589c0ff59fb5e2628a
Seif Hamada <seifhamada1@gmail.com>
2025-05-15 19:28:56 +0300

    CS50P Projects

---
+++ Intro to PY/Week 7/numb3rs/numb3rs/numb3rs.py
@@ -0,0 +1,18 @@
+import re
+
+
+
+def main():
+    print(validate(input("IPv4 Address: ")))
+
+
+def validate(ip):
+    pattern = "([1]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])"
+    address = re.search(r"^" + pattern + "\." + pattern + "\." + pattern + "\." + pattern + "$", ip)
+    if address:
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    main()
